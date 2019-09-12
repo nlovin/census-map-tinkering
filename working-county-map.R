e83f511c80f891679ceab0ae510407b1169581fa
Unknown <nlovin@umd.edu>
2019-09-11 20:39:41 -0400

    fixed object name

--- working-county-map.R
+++ working-county-map.R
@@ -55,7 +55,7 @@ us.proj <- "+proj=aea +lat_1=29.5 +lat_2=45.5 +lat_0=37.5 +lon_0=-96 +x_0=0 +y_0
 ##### ------------- Map ------------- ##### 
 
 ## Make the map
-work.map <- tm_shape(county.working.US, 
+work.map <- tm_shape(county.working, 
          projection = us.proj) +
   tm_fill("working.pct",
           palette = "PuBuGn",
