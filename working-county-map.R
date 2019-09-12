fc3359dd3769944019518f884ef81cddb7c7d850
Unknown <nlovin@umd.edu>
2019-09-11 20:38:32 -0400

    Figured out 'shift_geo' option in the tidycensus package
    Saved me from having to do it 'by hand' -- although I already started this process unfortunately.

--- working-county-map.R
+++ working-county-map.R
@@ -5,6 +5,7 @@ library(tidycensus)
 library(tidyverse)
 library(tmap)
 library(tmaptools)
+library(sf)
 library(rnaturalearth)
 
 ## Make sure census key is setup
@@ -14,7 +15,9 @@ library(rnaturalearth)
 
 ## Number of 16+ in the workforce
 county.working <- get_acs(geography = "county",
-                          variables = c("DP03_0002E"), geometry = T) %>% 
+                          variables = c("DP03_0002E"), 
+                          geometry = T, 
+                          shift_geo = TRUE) %>% 
   rename(working = estimate)
 
 head(county.working)
@@ -36,11 +39,6 @@ county.working <- county.working %>%
 ## Create state variable to use for filtering
 county.working$state <- str_extract(string = county.working$NAME ,pattern = '\\b[^,]+$')
 
-## Drop Alaska, Hawaii, etc.
-county.working <- county.working %>% 
-  filter(state != "Puerto Rico",
-         state != "Hawaii",
-         state != "Alaska")
 
 ##### ------------- Setup maps ------------- ##### 
 
@@ -57,11 +55,11 @@ us.proj <- "+proj=aea +lat_1=29.5 +lat_2=45.5 +lat_0=37.5 +lon_0=-96 +x_0=0 +y_0
 ##### ------------- Map ------------- ##### 
 
 ## Make the map
-work.map <- tm_shape(county.working, 
+work.map <- tm_shape(county.working.US, 
          projection = us.proj) +
   tm_fill("working.pct",
           palette = "PuBuGn",
-          title = "Active in labor force (%)", ) +
+          title = "% Working") +
   tm_shape(states_shp,
            projection = us.proj) +
   tm_borders() +
@@ -69,10 +67,11 @@ work.map <- tm_shape(county.working,
             legend.title.size = .8,
             legend.text.size = 0.5, 
             legend.position = c("LEFT", "BOTTOM"),
-            main.title.size = .8) +
+            main.title.size = .8, 
+            frame = F) +
   tm_credits("Source: 2013-2017 5-year American Community Survey | Measures include people 16 years and older", 
              position = c("RIGHT", "BOTTOM"),
-             size = .3)
+             size = .3) 
 
 
 ## save image
@@ -86,3 +85,52 @@ tmap_save(work.map, "working_county_map.png", width=1920, height=1080, asp=0)
 #"DP03_0002E" # in labor force 
 #"DP03_0001E" # total 16+
 
+
+
+##### ------------- Deprecated ------------- ##### 
+
+## Include Alaska and Hawaii
+
+## Projections
+AK.proj <- "+proj=aea +lat_1=55 +lat_2=65 +lat_0=50 +lon_0=-154 +x_0=0 +y_0=0 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs"
+HI.proj <- "+proj=aea +lat_1=8 +lat_2=18 +lat_0=13 +lon_0=-153 +x_0=0 +y_0=0 +ellps=GRS80 +datum=NAD83 +units=m +no_defs"
+
+## Map
+work.map.HI <- tm_shape(county.working.HI, 
+                        projection = HI.proj) +
+  tm_fill("working.pct",
+          palette = "PuBuGn", breaks = c(0,20,40,60,80,100)) + 
+  tm_legend(show = F) +
+  tm_layout(frame = F)
+
+work.map.AK <- tm_shape(county.working.AK, 
+                        projection = AK.proj) +
+  tm_fill("working.pct",
+          palette = "PuBuGn", breaks = c(0,20,40,60,80,100)) + 
+  tm_legend(show = F) +
+  tm_layout(frame = F)
+
+
+work.map
+print(work.map.HI, vp = grid::viewport(0.35, 0.1, width = 0.2, height = 0.1))
+print(work.map.AK, vp = grid::viewport(0.15, 0.15, width = 0.3, height = 0.3))
+
+#### Separate data
+## Drop Alaska, Hawaii, etc.
+county.working.AK <- county.working %>% 
+  filter(state == "Alaska")
+
+## Drop Alaska, Hawaii, etc.
+county.working.HI <- county.working %>% 
+  filter(state == "Hawaii") %>% 
+  st_crop(., c(xmin=-162.62, xmax=-153.87, ymin=18.05, ymax=23.68))
+
+## Drop Alaska, Hawaii, etc.
+county.working.PR <- county.working %>% 
+  filter(state == "Puerto Rico")
+
+## Drop Alaska, Hawaii, etc.
+county.working.US <- county.working %>% 
+  filter(state != "Puerto Rico",
+         state != "Hawaii",
+         state != "Alaska")
